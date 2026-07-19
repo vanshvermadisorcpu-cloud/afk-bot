f28220be70434d97660c1adeda8499553c9b81b9
vanshvermadisorcpu-cloud <vanshvermadisorcpu@gmail.com>
2026-07-19 12:58:05 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const mineflayer = require('mineflayer')
 const bot = mineflayer.createBot({
   host: 'PB36-SMP1.aternos.me',
   port: 24464,
-  username: 'AFK_Bot',
+  username: 'SHAANKRAAAAAAA',
   version: false
 })
 
@@ -11,8 +11,10 @@ bot.on('spawn', () => {
   console.log('Bot joined!')
 })
 
+bot.on('error', (err) => {
+  console.log('Error:', err)
+})
+
 bot.on('end', () => {
   console.log('Disconnected!')
 })
-
-bot.on('error', console.log)
